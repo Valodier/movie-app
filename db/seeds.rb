a9102b3627f414d6c51aeaf6ece2aa470c2bcc24
William Meyer <williamjmiii@gmail.com>
2022-03-11 11:28:42 -0500

    created instances in seeds.rb, create movie controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,11 @@
 
 # actor = Actor.new({first_name: "Lee", last_name: "Pace", known_for: "Soldiers Girl"})
 # actor.save
+movie4 = Movie.new ({title:"Elf", year:"1700", plot:"A perfectly sane man raised by actual elves and actual Santa tries to convince everyone that Santa is, in fact, real."})
+movie4.save
+
+movie5 = Movie.new ({title:"Action Person", year:"1500", plot:"A small boy is transported into his favorite action film"})
+movie5.save
+
+movie6 = Movie.new ({title:"Dumb and Dumber", year:"3000", plot:"A couple of dimwitted friends set out to return a million dollars they find while also spending the million dollars"})
+movie6.save
