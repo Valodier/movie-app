eaba5b91b6ba0e9a1cb80838932246b6b21fa7f8
William Meyer <williamjmiii@gmail.com>
2022-03-12 11:55:29 -0500

    reseeded DB, commented out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,23 @@
 # actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
 # actor.save
 
-# actor = Actor.new({first_name: "Will", last_name: "Farrell", known_for: "Elf"})
-# actor.save
+# actor1 = Actor.new({first_name: "Will", last_name: "Farrell", known_for: "Elf"})
+# actor1.save
 
-# actor = Actor.new({first_name: "Edward", last_name: "Norton", known_for: "Fight Club"})
-# actor.save
+# actor2 = Actor.new({first_name: "Edward", last_name: "Norton", known_for: "Fight Club"})
+# actor2.save
 
-# actor = Actor.new({first_name: "Lee", last_name: "Pace", known_for: "Soldiers Girl"})
-# actor.save
+# actor3 = Actor.new({first_name: "Lee", last_name: "Pace", known_for: "Soldiers Girl"})
+# actor3.save
+
 # movie4 = Movie.new ({title:"Elf", year:"1700", plot:"A perfectly sane man raised by actual elves and actual Santa tries to convince everyone that Santa is, in fact, real."})
+
 # movie4.save
 
 # movie5 = Movie.new ({title:"Action Person", year:"1500", plot:"A small boy is transported into his favorite action film"})
+
 # movie5.save
 
 # movie6 = Movie.new ({title:"Dumb and Dumber", year:"3000", plot:"A couple of dimwitted friends set out to return a million dollars they find while also spending the million dollars"})
+
 # movie6.save
--- test.rb
+++
@@ -1,9 +0,0 @@
-require "http"
-
-system "clear"
-
-puts "Please enter the ID for the movie you would like to see"
-
-response = HTTP.get("http://localhost:3000/movies")
-
-pp response.parse(:json)
