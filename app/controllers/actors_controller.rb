7a106330e07bc8b10dd74657f63f5d543c49eedb
William Meyer <williamjmiii@gmail.com>
2022-03-15 09:39:56 -0400

    changed [] ro () in show and update for find_by methods

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,7 +8,7 @@ class ActorsController < ApplicationController
     actor = Actor.new(
       first_name: params["first_name"],
       last_name: params["last_name"],
-      known_for: params ["known_for"]
+      known_for: params["known_for"]
     )
 
     actor.save
@@ -17,13 +17,13 @@ class ActorsController < ApplicationController
 
   def show
     actor_id = params[:id]
-    show_id = Actor.find_by[id: actor_id]
+    show_id = Actor.find_by(id: actor_id)
     render json: show_id.as_json
   end
 
   def update
     actor_id = params[:id]
-    actor = Actor.find_by[id: actor_id]
+    actor = Actor.find_by(id: actor_id)
     
     actor.first_name = params["first_name"] || actor.first_name
     actor.last_name = params["last_name"] || actor.last_name
@@ -38,7 +38,7 @@ class ActorsController < ApplicationController
     actor = Actor.find_by[id: actor_id]
     actor.destroy
 
-    render json: {"MY GOD WHAT HAVE YOU DONE?!"}
+    render json: {message: "MY GOD WHAT HAVE YOU DONE?!"}
   end
   
 
