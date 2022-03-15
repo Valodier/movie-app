44a1976de2bb5ac6f96091e610ff47ca6bd25687
William Meyer <williamjmiii@gmail.com>
2022-03-15 09:31:38 -0400

    added create action to Actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -5,9 +5,14 @@ class ActorsController < ApplicationController
   end
 
   def create
+    actor = Actor.new(
+      first_name = params["first_name"],
+      last_name = params["last_name"],
+      known_for = params ["known_for"]
+    )
 
-
-    
+    actor.save
+    render json: actor.as_json
   end
 
   def show
