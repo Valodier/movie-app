c1be8c5967859428158a7d3eee7db45d0584d594
William Meyer <williamjmiii@gmail.com>
2022-03-15 09:57:29 -0400

    added empty actions for RESTful actions to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,25 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+    render json: movies.as_json
+  end
+
+  def method_name
+    
+  end
+
   def show
     movie_id = params["id"]
     movie = Movie.find_by(id: movie_id)
     render json: movie.as_json
   end
 
-  def index
-    movies = Movie.all
-    render json: movies.as_json
+  def method_name
+    
   end
+  
+  def method_name
+    
+  end
+
 end
