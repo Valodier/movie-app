bfa838c8bc86ef5e67824d4f68b1f09a84a288ad
William Meyer <williamjmiii@gmail.com>
2022-03-15 09:11:01 -0400

    Cleared actions from actor controller, added empty actions for each RESTful action

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,24 @@
 class ActorsController < ApplicationController
   
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+  
+
 end
