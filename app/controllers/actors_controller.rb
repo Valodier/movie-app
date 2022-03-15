2276ca46d296b8c89881d552ae0d02131d858a6b
William Meyer <williamjmiii@gmail.com>
2022-03-15 09:27:12 -0400

    added show action to Actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,20 +1,22 @@
 class ActorsController < ApplicationController
   
   def index
-    
+    render json: Actor.all.as_json
   end
 
   def create
+
     
   end
 
   def show
-    
+    actor_id = params[:id]
+    show_id = Actor.find_by[id: actor_id]
+    render json: show_id.as_json
   end
 
   def update
-    
-  end
+  
 
   def destroy
     
