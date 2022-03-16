0902c3f7c83e8fe8fec7a424c4a556ee4be0fde4
William Meyer <williamjmiii@gmail.com>
2022-03-16 18:30:52 -0400

    added .json to end of URLs in request.http, add instance variable to index action for index view, added id to index view

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,11 +1,12 @@
 class ActorsController < ApplicationController
   
   def index
-    render json: Actor.all.as_json
+    @actors = Actor.all
+    render :index
   end
 
   def create
-    actor = Actor.new(
+    @actor = Actor.new(
       first_name: params["first_name"],
       last_name: params["last_name"],
       known_for: params["known_for"],
@@ -13,8 +14,8 @@ class ActorsController < ApplicationController
       age:params["age"]
     )
 
-    actor.save
-    render json: actor.as_json
+    @actor.save
+    render :show
   end
 
   def show
@@ -25,16 +26,16 @@ class ActorsController < ApplicationController
 
   def update
     actor_id = params[:id]
-    actor = Actor.find_by(id: actor_id)
+    @actor = Actor.find_by(id: actor_id)
     
-    actor.first_name = params["first_name"] || actor.first_name
-    actor.last_name = params["last_name"] || actor.last_name
-    actor.known_for = params["known_for"] || actor.known_for
-    actor.gender = params["gender"] || actor.gender
-    actor.age = params["age"] || actor.age
+    @actor.first_name = params["first_name"] || @actor.first_name
+    @actor.last_name = params["last_name"] || @actor.last_name
+    @actor.known_for = params["known_for"] || @actor.known_for
+    @actor.gender = params["gender"] || @actor.gender
+    @actor.age = params["age"] || @actor.age
 
-    actor.save
-    render json: actor.as_json
+    @actor.save
+    render :show
   end
 
   def destroy
