ab3097c0f9296c31339cbb628d7080dd152db890
William Meyer <williamjmiii@gmail.com>
2022-03-16 17:08:57 -0400

    added director and language migrations, migrated to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_11_153424) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_16_210757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_11_153424) do
     t.text "plot"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
+    t.string "language"
   end
 
 end
