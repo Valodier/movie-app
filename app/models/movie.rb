a10fbf28581384795716b09c8822a074dedd429c
William Meyer <williamjmiii@gmail.com>
2022-03-17 17:45:05 -0400

    Commented out validations for testing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,8 @@
 class Movie < ApplicationRecord
-  validates :title, length: {minimum: 2}
-  validates :title, presence: true
-  validates :year, length: {minimum: 4}
-  validates :year, presence: true
-  validates :director, presence: true
-  validates :director, length: {minimum: 2}
+  # validates :title, length: {minimum: 2}
+  # validates :title, presence: true
+  # validates :year, length: {minimum: 4}
+  # validates :year, presence: true
+  # validates :director, presence: true
+  # validates :director, length: {minimum: 2}
 end
