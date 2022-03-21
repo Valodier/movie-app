37a70e91987f8091e30b7c767d134e0dd53ed39e
William Meyer <williamjmiii@gmail.com>
2022-03-21 17:26:35 -0400

    updated actors controller create/update with new movie_id foreign key

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -11,7 +11,8 @@ class ActorsController < ApplicationController
       last_name: params["last_name"],
       known_for: params["known_for"],
       gender:params["gender"],
-      age:params["age"]
+      age:params["age"],
+      movie_id:params["movie_id"]
     )
     if @actor.save
       render :show
@@ -36,6 +37,7 @@ class ActorsController < ApplicationController
     @actor.known_for = params["known_for"] || @actor.known_for
     @actor.gender = params["gender"] || @actor.gender
     @actor.age = params["age"] || @actor.age
+    @actor.movie_id = params["movie_id"] || @actor.movie_id
 
     if @actor.save
       render :show
