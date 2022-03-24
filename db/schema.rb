b94a130653a07fe4bc5fc02111bcf4a2f129c695
William Meyer <williamjmiii@gmail.com>
2022-03-24 07:23:16 -0400

    adds admin:boolean, default:false to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_24_104012) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_24_112229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_24_104012) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
