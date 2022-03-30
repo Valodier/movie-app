8ee3fc6e22b4bc1488b80712afadefa5a5846092
William Meyer <valodier@icloud.com>
2022-03-30 12:14:25 -0400

    adds filled create function

--- app/controllers/movie_genres_controller.rb
+++ app/controllers/movie_genres_controller.rb
@@ -1,2 +1,19 @@
 class MovieGenresController < ApplicationController
+
+  def create
+    @movie_genre = MovieGenre.new(
+      movie_id: params["movie_id"],
+      genre_id: params["genre_id"]
+    )
+    
+    if @movie_genre.save
+      render :index
+    else
+      render json: {message: @movie_genre.errors.full_messages},
+      status: 422
+
+    end
+
+  end
+
 end
